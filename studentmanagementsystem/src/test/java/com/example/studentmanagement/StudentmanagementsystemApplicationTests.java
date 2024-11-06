6880a296194b7c1b31e103d5aa01fa2079c72a75
marthamutsvandiani <122863685+marthamutsvandiani@users.noreply.github.com>
2024-11-06 21:30:05 +0200

    Update StudentmanagementsystemApplicationTests.java

--- studentmanagementsystem/src/test/java/com/example/studentmanagement/StudentmanagementsystemApplicationTests.java
+++ studentmanagementsystem/src/test/java/com/example/studentmanagement/StudentmanagementsystemApplicationTests.java
@@ -2,12 +2,13 @@ package com.example.studentmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")  // Activates the 'test' profile for this test class
 class StudentmanagementsystemApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
